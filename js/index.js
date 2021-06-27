eaf0520503f4e293b21b468ff60e800e9fdac7da
Edinelson Lima <edinelsonlima86@gmail.com>
2021-06-27 11:41:17 -0300

    Finalizado

--- js/index.js
+++ js/index.js
@@ -1,18 +1,36 @@
+// Function para alterar a cor da Navbar quando a mesma não estiver dentro do header
+window.addEventListener('scroll', navBarColor)
+function navBarColor() {
+  let wH = window.innerHeight;
+  let nav = document.querySelector('nav')
 
-
-function initMap() {
-    var coordenadas = {lat: -5.047545025626878, lng: -42.75877203768939};
-
-    map = new google.maps.Map(document.getElementById('mapa'), {
-      zoom: 11,
-      center: coordenadas 
-    });
-
-    // // var marker = new google.maps.Marker({
-    // //   position: coordenadas,
-    // //   map: mapa,
-    // //   title: 'Meu marcador'
-    // // });
+  if (scrollY > wH - 50) {
+    nav.style.background = "#2b4068"
+  }
+  else {
+    nav.style.background = '#2d75fb0c'
   }
 
-  initMap()
+}
+
+// Function para retornar o menu hamburger após o click em uma ancora
+let input = document.querySelector("input#checkbox")
+let li = document.querySelectorAll('nav ul li a')
+li.forEach(function (item){
+  item.onclick = () =>{
+    input.checked = false
+  }
+})
+
+// Function para chamar o mmapa da API google maps
+let map
+function initMap() {
+  var coordenadas = { lat: -5.047545025626878, lng: -42.75877203768939 };
+
+  map = new google.maps.Map(document.getElementById('mapa'), {
+    zoom: 11,
+    center: coordenadas
+  });
+}
+
+initMap()
